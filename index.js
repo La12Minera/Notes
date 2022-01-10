898e4fcae5d9469e62b78f87d5a6d93a686ee1b5
FedericoAbeldaño <a.fede73@hotmail.com>
2022-01-10 14:18:14 -0300

    Change Port

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.post('/api/notes/', (req, resp) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
